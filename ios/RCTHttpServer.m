6da0f97a434b74770052ea2223fb174d66bf50fb
Robert Herber <robert@herber.me>
2018-06-12 21:05:20 +0200

    Added get to ios

--- ios/RCTHttpServer.m
+++ ios/RCTHttpServer.m
@@ -34,6 +34,40 @@ RCT_EXPORT_METHOD(start:(NSInteger) port
     dispatch_sync(dispatch_get_main_queue(), ^{
         _webServer = [[WGCDWebServer alloc] init];
 
+        [_webServer addDefaultHandlerForMethod:@"GET"
+                    requestClass:[WGCDWebServerDataRequest class]
+                    processBlock:^WGCDWebServerResponse *(WGCDWebServerRequest* request) {
+
+            WGCDWebServerDataRequest* dataRequest = (WGCDWebServerDataRequest*)request;
+            WGCDWebServerDataResponse* _requestResponse;
+                        
+            NSMutableString *requestId = [NSMutableString stringWithCapacity: lengthOfRandomString];
+            
+            for (int i=0; i<lengthOfRandomString; i++) {
+                [requestId appendFormat: @"%C", [letters characterAtIndex: arc4random_uniform([letters length])]];
+            }
+                    
+                    
+                        
+            [self.bridge.eventDispatcher sendAppEventWithName:@"httpServerResponseReceived"
+                                                         body:@{
+                                                             @"requestId": requestId,
+                                                                @"method": @"GET",
+                                                                @"headers": dataRequest.headers,
+                                                                @"query":
+                                                                    dataRequest.query,
+                                                                     @"url": dataRequest.URL.relativeString}];
+            
+            while (_requestResponse == NULL) {
+                _requestResponse = [_requestResponses objectForKey:requestId];
+                [NSThread sleepForTimeInterval:0.02f];
+            }
+                        
+            [_requestResponses removeObjectForKey:requestId ];
+
+            return _requestResponse;
+        }];
+
         [_webServer addDefaultHandlerForMethod:@"POST"
                     requestClass:[WGCDWebServerDataRequest class]
                     processBlock:^WGCDWebServerResponse *(WGCDWebServerRequest* request) {
@@ -52,6 +86,7 @@ RCT_EXPORT_METHOD(start:(NSInteger) port
             [self.bridge.eventDispatcher sendAppEventWithName:@"httpServerResponseReceived"
                                                          body:@{@"postData": dataRequest.jsonObject,
                                                                 @"requestId": requestId,
+                                                                @"method": @"POST",
                                                                 @"headers": dataRequest.headers,
                                                                 @"query":
                                                                     dataRequest.query,
